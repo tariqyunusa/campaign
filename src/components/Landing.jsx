1c22a32deb770ae5e762b992623e8dabc221c02d
tariq yunusa <tariikhyunusa@gmail.com>
2023-09-14 19:12:48 +0100

    UI Update

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import video from "../assets/video.mp4";
+import "../styles/landing.css";
 
-const landing = () => {
+const landing = ({ setIsHoveringLink }) => {
+  const handleMouseEnter = () => {
+    setIsHoveringLink(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHoveringLink(false);
+  };
+
   return (
     <section className="conatiner">
       <div className="main_text">
-        <h2>Where The Genius Of History Meets The Vision Of Tomorrow.</h2>
+        <h1>Where The Genius Of History Meets The Vision Of Tomorrow.</h1>
+      </div>
+      <div className="video">
+        <video
+          src={video}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          autoPlay
+          loop
+          muted
+        />
       </div>
     </section>
   );
