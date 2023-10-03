b28ff33a560a52d276351dfd19d32e30585cddd6
tariq yunusa <tariikhyunusa@gmail.com>
2023-10-03 13:22:05 +0100

    added animation to the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,51 @@ gsap.registerPlugin(ScrollTrigger);
 const About = () => {
   const aboutHeaderRef = useRef(null);
   const RightSideH3Ref = useRef(null);
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: aboutHeaderRef.current,
+          start: "top -300px", // Adjust this value as needed
+        },
+      })
+      .to(aboutHeaderRef.current, {
+        delay: 0.5,
+        y: -20,
+        clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+        duration: 3.5,
+      });
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: RightSideH3Ref.current,
+          start: "top -100px", // Adjust this value as needed
+        },
+      })
+      .to(RightSideH3Ref.current, {
+        delay: 0.5,
+        y: -20,
+        clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+        duration: 3.5,
+      })
+      .to(
+        textRef.current,
+        {
+          y: -20,
+          clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+          duration: 2.5,
+        },
+        "<"
+      );
+  }, []);
 
   return (
     <>
       <div className="about">
         <div className="about_header">
-          <h1 className="about_h1">
+          <h1 className="about_h1" ref={aboutHeaderRef}>
             INCORPORATE WISDOM AND BRILLIANCE OF THE PAST WITH FAST FORWARD
             THINKING AND AN INNOVATIVE VISION FOR THE FUTURE
           </h1>
@@ -28,7 +67,7 @@ const About = () => {
               </h3>
             </div>
 
-            <p>
+            <p ref={textRef} className="text_about">
               we are a Nigerian Based Creative Agency whose niche is drawing
               inspiration from the great minds of history while applying that
               wisdom to contemporary and future creative endeavours This
