b07d4fb488cbbb0322ec38f5782e26a73b586001
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-02-28 15:36:00 +0100

    fixed conditional render to display video

--- src/components/VideoComponent.jsx
+++ src/components/VideoComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import placeholder from "../assets/placeholder.jpg"
 import "../styles/VideoComponent.css"
@@ -12,10 +12,12 @@ const VideoComponent = ({src, setIsHoveringLink }) => {
       const handleMouseLeave = () => {
         setIsHoveringLink(false);
       };
-
-    const handleVideo = () => {
+      const handleVideoLoad = () => {
         setIsVideoLoaded(true)
-    }
+      }
+    useEffect(() => {
+      setIsVideoLoaded(true)
+    },[])
   return (
     <div className='video_container'>
         {!isVideoLoaded ? <img src={placeholder} className='placeholder'/> : <video
@@ -26,7 +28,7 @@ const VideoComponent = ({src, setIsHoveringLink }) => {
           autoPlay
           loop
           muted
-          onLoadedData={handleVideo}
+          onLoadedData={handleVideoLoad}
         />}  
         
     </div>
